351df5ed43b94f4e37bd80edb64e9ba64385694e
Aaditya Narain <aaditya.narain.avna@gmail.com>
2025-08-23 21:58:09 +0530

    take home setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/notesRouter.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config(); // Load .env variables
 
@@ -13,9 +14,17 @@ app.use("/api/notes", router);
 const PORT = process.env.PORT || 5001;
 
 // Only start the server if NOT in test mode
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "test") 
+{
+
+  //connectDB().then(() => {
+  //  app.listen(PORT, () => {
+  //    console.log("Server started on PORT:", PORT);
+  //  });
+  //});
+
   app.listen(PORT, () => {
-    console.log(`Server started at Port ${PORT}`);
+    console.log("Server started on PORT:", PORT);
   });
 }
 
