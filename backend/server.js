7804278104da68d71c44d6d5d752f7066d43c61b
Aaditya Narain <aaditya.narain.avna@gmail.com>
2025-08-23 12:08:47 +0530

    First Api

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app= express();
 
+app.get("/api/notes", (req, res) => {
+    res.send("You got some notes");
+});
+
 app.listen(5001,() => {
     console.log("Server started at Port 5001");
 });
